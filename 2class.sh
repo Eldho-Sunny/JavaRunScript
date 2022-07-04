5d970384848a7ec9b33c3a1d8801bb9482a046ed
Eldho-Sunny <eldho832@gmail.com>
2022-07-04 21:34:16 +0530

    Initial version of 2class

---
+++ 2class.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo -n Enter main class::
+read x1;
+echo -n Enter class::
+read x2;
+echo
+ecj $x.java && dx --dex --output=$x1.dex $x2.dex $x1.class $x2.class && dalvikvm -cp $x1.dex $x2.dex $x1 $x2;
+echo

7e7bd684065fd30049b3dc93604fa50a9a9a73df
ES <eldho832@gmail.com>
2022-07-16 16:47:40 +0530

    removed typo

--- 2class.sh
+++ 2class.sh
@@ -4,5 +4,5 @@ read x1;
 echo -n Enter class::
 read x2;
 echo
-ecj $x.java && dx --dex --output=$x1.dex $x2.dex $x1.class $x2.class && dalvikvm -cp $x1.dex $x2.dex $x1 $x2;
+ecj $x2.java && dx --dex --output=$x1.dex $x2.dex $x1.class $x2.class && dalvikvm -cp $x1.dex $x2.dex $x1 $x2;
 echo
